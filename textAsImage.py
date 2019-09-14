4defdf2596070d55fc23e427d656a4728cd482ba
Ben Li <benji112358@gmail.com>
2019-09-14 11:20:15 -0400

    hardware readable file format

--- textAsImage.py
+++ textAsImage.py
@@ -14,16 +14,17 @@ import turtle
 (2) shift all x-coords rightwards by charSpace*numCharsDrawn
 '''
 
+
 def generateCoordinates(inputText, scale=5):
 
 	# LUT
 	charLUT = {
 		' ': [[0,0]], #space
-		'A': [[0,0], 'D', [0,10], [10,10], [10,5], [0,5], [10,5], [10,0], 'L'],
-		'B': [[0,0], 'D', [0,10], [10,10], [10,5], [0,5], [10,5], [10,0], [0,0], 'L'],
-		'C': [[0,0], 'D', [0,10], [10,10], [0,10], [0,0], [10,0], 'L'],
-		'D': [[0,0], 'D', [0,10], [10,10], [10,0], [0,0], 'L'],
-		'E': [[0,0], 'D', [0,10], [10,10], [0,10], [0,5], [10,5], [0,5], [0,0], [10,0], 'L']
+		'A': [[0,0], 'P1', [0,10], [10,10], [10,5], [0,5], [10,5], [10,0], 'P0'],					# P0 :: turn off laser
+		'B': [[0,0], 'P1', [0,10], [10,10], [10,5], [0,5], [10,5], [10,0], [0,0], 'P0'],			# P1 :: turn ON laser
+		'C': [[0,0], 'P1', [0,10], [10,10], [0,10], [0,0], [10,0], 'P0'],
+		'D': [[0,0], 'P1', [0,10], [10,10], [10,0], [0,0], 'P0'],
+		'E': [[0,0], 'P1', [0,10], [10,10], [0,10], [0,5], [10,5], [0,5], [0,0], [10,0], 'P0']
 	}
 
 	# return coordinates for drawing inputText
@@ -32,7 +33,7 @@ def generateCoordinates(inputText, scale=5):
 	numCharsDrawn = 0			# counter
 	for char in inputText:
 		for coord in charLUT[char.upper()]:
-			if coord in ['D','L']:	# element is a lift/drop flag
+			if coord in ['P0','P1']:	# element is a lift/drop flag
 				drawingSequence.append(coord)
 				continue
 			else: # element is coord
@@ -52,14 +53,27 @@ def turtleTest(coordSequence):
 	pencil = turtle.Turtle()
 
 	for coord in coordSequence:
-		if coord == 'D': pencil.pendown()	# put pen on canvas @ start of char
-		elif coord == 'L': pencil.penup()	# lift pen off canvas @ end of char
+		if coord == 'P1': pencil.pendown()	# put pen on canvas @ start of char
+		elif coord == 'P0': pencil.penup()	# lift pen off canvas @ end of char
 		else:
 			pencil.goto(coord[0], coord[1])
 	window.exitonclick()
 
-# Write sequence to Text-File
-def writeCoordinateFile(coordSequence):
-	
 
-turtleTest(generateCoordinates("A BCD ABC"))
+# Generate file for serial-transmission to Microcontroller
+def writeCoordsToFile(coordSequence):
+
+	# write coords + newline
+	f = open('textCoordinates.txt', 'w+')
+	for coord in coordSequence:
+		command = coord
+		if coord not in ['P0', 'P1']: 
+			command = 'X'+str(coord[0]) + ' Y'+str(coord[1])
+		f.write(command+'\n')
+
+	# append 'D' to ensure file is deleted off uC
+	f.write('D\n')
+
+
+textCoordinates = generateCoordinates("A BCD ABC")
+writeCoordsToFile(textCoordinates)
